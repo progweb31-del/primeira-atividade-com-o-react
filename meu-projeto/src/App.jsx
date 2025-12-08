6ccd626538c58d43d12a5b2e6022008a89567b61
prog <progweb31@gmail.com>
2025-12-08 17:10:41 -0300

    mudei bastante coisa

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -3,82 +3,26 @@ import React, { useState } from 'react';
 
 // Importa os componentes que serão usados na página
 import Tabela from './components/Tabela';
-import ModalConfirmacao from './components/ModalConfirmacao'
-
 
 // Importa o estilo CSS geral do site
 import './App.css';
 
 function App() {
-  // Estado com a lista de visitantes
-  // começa como lista vazia (array)
-  const [visitantes, setVisitantes] = useState([]);
-
-  // Estado para mostrar/ocultar o modal
-  const [mostrarModal, setMostrarModal] = useState(false);
-
-  // Índice de quem será excluído
-  const [indiceParaExcluir, setIndiceParaExcluir] = useState(null);
-
-  //Novo estado para mensagem de sucesso
-  const [mensagemSucesso, setMensagemSucesso] = useState('');
-
-  //*****A partir daqui a execução começa pelo return
-
-  // Função para adicionar visitante
-  // Usa o operador spread (...visitantes), que copia todos os visitantes já cadastrados  
-  function adicionarVisitante(nome, idade) {
-    setVisitantes([...visitantes, { nome, idade }]); // Cria uma nova lista, com todos os anteriores + o novo e atualiza a tela (set).
-
-    //Mostra mensagem de sucesso
-    setMensagemSucesso('Visitante adicionado com sucesso!');
-
-    //Limpa a mensagem após 2 segundos
-    setTimeout(() => {
-      setMensagemSucesso('');
-    }, 2000);
-  }
-
-  // Solicita exclusão (abre modal: janela)
-  function solicitarExclusao(index) {
-    setIndiceParaExcluir(index);
-    setMostrarModal(true);//mostra a tela do modal
-    //Vai para a linha 100 "{mostrarModal &&":Se mostrarModal for true,
-    //então exiba o componente <ModalConfirmacao />."
-  }
-
-  // Confirma a exclusão
-  function confirmarExclusao() {
-    //cria uma nova lista, chamada novaLista, que contém todos os visitantes, menos o que está no índice que queremos excluir.
-    //.filter(...) É uma função que filtra a lista, deixando passar só os itens que você quiser manter (tira o indiceparaexcluir).
-    //(_, i) => i !== indiceParaExcluir. Função de teste usada pelo filter.
-    //O _ é o próprio item da lista (nome, idade), mas como não vamos usar o item aqui (apenas o índice). usamos _ (é só um costume para dizer: "não estou usando esse parâmetro").
-    //O i é o índice do item na lista (0, 1, 2, ...).
-    //A expressão i !== indiceParaExcluir significa: Só quero manter os itens que NÃO estão no índice que quero excluir.
-    //o indiceParaExcluir é informado na function solicitarExclusao
-    const novaLista = visitantes.filter((_, i) => i !== indiceParaExcluir);
-    setVisitantes(novaLista);
-    setMostrarModal(false);
-    setIndiceParaExcluir(null);
-  }
-
-  // Cancela exclusão
-  function cancelarExclusao() {
-    setMostrarModal(false);
-    setIndiceParaExcluir(null);
-  }
 
   return (
     <div>
+      <div className='sla'>
       <h1>Bem-vindo a loja de roupas</h1>
-
+      <p>inicio</p>
+      <p>sobre</p>
+      <p>contato</p>
+      </div>
       <p>Neste lugar voce encontrará variedades de roupas, acessorios, calçados, etc...</p>
       {/*Exibe a mensagem de sucesso, se existir */}
       {/*mensagemSucesso &&= Verifica se a variável mensagemSucesso tem algum valor (true, texto, etc.).
       Caso tenha, mostra um parágrafo com a mensagem e estilo CSS*/}
-      {mensagemSucesso && <p className="mensagem-sucesso">{mensagemSucesso}</p>}
       
-      <Tabela dados={visitantes} onRemover={solicitarExclusao} />
+      <Tabela/>
       {/* Formulário para adicionar visitantes */}
       {/* Formulário para adicionar visitantes */}
       {/* Quando um componente (como <Formulario />) precisa usar uma função que está declarada em outro componente (como App.jsx), é necessário **passar essa função como uma props**. Isso acontece porque o componente filho **não tem acesso direto** às funções do componente pai.*/}
@@ -96,14 +40,7 @@ function App() {
       {/* Imagem ilustrativa */}
    
       {/* Modal (janela) de confirmação */}
-      {mostrarModal && (
-        <ModalConfirmacao
-          mensagem="Tem certeza que deseja excluir este visitante?"
-          onConfirmar={confirmarExclusao}
-          onCancelar={cancelarExclusao}
-        />
-      )}
-      
+
     </div>
   );
 
--- meu-projeto/src/components/Tabela.jsx
+++ meu-projeto/src/components/Tabela.jsx
@@ -1,6 +1,5 @@
-// Importa o React e o ícone de lixeira da biblioteca react-icons
+
 import React from 'react';
-import { FaTrash } from 'react-icons/fa'; // Fa = FontAwesome
 import Imagem from './Imagem';
 import Imagem3 from './Imagem3';
 import Imagem2 from './Imagem2';
@@ -8,54 +7,46 @@ import Imagem4 from '../components/terno'
 import Imagem5 from './aneis';
 import Imagem6 from './moletom';
 
-import './tabela.css'
+import styles from './tabela.module.css';
 
 function Tabela() {
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Roupas</th>
-          <th>Tamanho</th>
-          <th>preço</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-            <td>Camisas sociais <Imagem/> camisas dedicadas para a maior parte das ocasioes</td>
-            <td>medio</td>
-            <td>R$80</td>
-        </tr>
-        <tr>
-          <td>Sapatos <Imagem3/></td>
-          <td>39</td>
-          <td>R$150</td>
-        </tr>
-        <tr>
-          <td>Camisas (oversized)<Imagem2/>
-          </td>
-          <td>grande (XL)</td>
-          <td>R$35</td>
-        </tr>
-        <tr>
-          <td>Moletom <Imagem6/></td>
-          <td>medio</td>
-          <td>R$70</td>
-        </tr>
-        <tr>
-          <td>Terno e calça slim fit <Imagem4/>
-          </td>
-          <td>variado (sob medida)</td>
-          <td>R$820</td>
-        </tr>
-        <tr>
-          <td>Kit de aneis de ouro<Imagem5/></td>
-          <td>ajustável</td>
-          <td>R$14 (50% off)</td>
-        </tr>
-      </tbody>
-    </table>
+  return ( <>
+  <h1>roupas diversas</h1>
+    <div className={styles.container}>
+     
+    <div className={styles.divs}>
+            <h1>Camisas sociais <Imagem/> camisas dedicadas para a maior parte das ocasioes</h1>
+            <p>medio</p>
+            <p>R$80</p>
+    </div>
+   <div className={styles.divs}>
+          <h1>Sapatos <Imagem3/></h1>
+          <p>39</p>
+          <p>R$150</p>
+    </div>
+    <div className={styles.divs}>
+          <h1>Camisas (oversized)<Imagem2/></h1>
+          <p>grande (XL)</p>
+          <p>R$35</p>
+    </div>
+    <div className={styles.divs}>   
+          <h1>Moletom <Imagem6/></h1>
+          <p>medio</p>
+          <p>R$70</p>
+    </div>
+    <div className={styles.divs}>    
+          <h1>Terno e calça slim fit <Imagem4/></h1>
+          <p>variado (sob medida)</p>
+          <p>R$820</p>
+    </div>
+    <div className={styles.divs}>     
+          <h1>Kit de aneis de ouro<Imagem5/></h1>
+          <p>ajustável</p>
+          <p>R$14 (50% off)</p>
+    </div>  
+    </div>
+    </>
   )
 }
 
