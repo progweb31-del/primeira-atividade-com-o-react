d197b69aec88975bf275bed4806f00c43928cbec
prog <progweb31@gmail.com>
2025-12-10 14:34:56 -0300

    dnv alterei um bocado de coisa

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -1,6 +1,5 @@
 // Importa o React e a função useState para controlar estados
 import React, { useState } from 'react';
-
 // Importa os componentes que serão usados na página
 import Tabela from './components/Tabela';
 
@@ -12,34 +11,14 @@ function App() {
   return (
     <div>
       <div className='sla'>
-      <h1>Bem-vindo a loja de roupas</h1>
-      <p>inicio</p>
-      <p>sobre</p>
-      <p>contato</p>
+      <div className='p0'><h1>Bem-vindo a loja de roupas</h1></div>
+      <div className='p1'><p>inicio</p></div>
+      <div className='p2'><p>sobre</p></div>
+      <div className='p3'><a href=""></a></div>
       </div>
       <p>Neste lugar voce encontrará variedades de roupas, acessorios, calçados, etc...</p>
-      {/*Exibe a mensagem de sucesso, se existir */}
-      {/*mensagemSucesso &&= Verifica se a variável mensagemSucesso tem algum valor (true, texto, etc.).
-      Caso tenha, mostra um parágrafo com a mensagem e estilo CSS*/}
-      
-      <Tabela/>
-      {/* Formulário para adicionar visitantes */}
-      {/* Formulário para adicionar visitantes */}
-      {/* Quando um componente (como <Formulario />) precisa usar uma função que está declarada em outro componente (como App.jsx), é necessário **passar essa função como uma props**. Isso acontece porque o componente filho **não tem acesso direto** às funções do componente pai.*/}
-      {/* Neste exemplo, estamos passando a função "adicionarVisitante", que foi criada no App.jsx, para o componente <Formulario /> por meio de uma props chamada "onAdicionar".
-      ➤ O nome "onAdicionar" não é uma palavra reservada, você pode escolher qualquer nome! Mas, por **convenção**, usamos nomes como "onAdicionar", "onClick", "onChange", etc., porque indicam que estamos passando uma função que será executada em algum evento.*/}
-    
 
-      {/* Tabela com os visitantes */}
-      {/*dados={visitantes} É uma prop chamada dados que está enviando 
-      para Tabela os dados (lista) que estão guardados na variável visitantes. 
-      A Tabela vai usar esses dados para montar as linhas com nome e idade.*/}
-      {/*onRemover={solicitarExclusao} é uma prop chamada onRemover, que enviando 
-      uma função chamada solicitarExclusao para a Tabela." */}
-      
-      {/* Imagem ilustrativa */}
-   
-      {/* Modal (janela) de confirmação */}
+      <Tabela/>
 
     </div>
   );
--- meu-projeto/src/Contato.jsx
+++ meu-projeto/src/Contato.jsx
@@ -3,24 +3,10 @@ import {BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const contato = () =>(
-    
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla laborum officiis quaerat, hic non fuga.</h1>
-
-)
-
-
-
-const MyComponent = () => {
-  const navigate = useNavigate();
-
-  const goToAbout = () => {
-    navigate('./App.jsx');
-  };
-
+function contato(){
   return (
-    <div>
-      <button onClick={goToAbout}>Ir para About</button>
-    </div>
-  );
-};
+    <p>kjsnkjnlakjnjkasns </p>
+  )
+}
+
+export default contato;
